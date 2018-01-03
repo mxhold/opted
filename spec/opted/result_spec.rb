f69e36d71c56f4bc4824129cdf64202106ed11cf
Max Holder <mxhold@gmail.com>
2018-01-03 11:52:57 -0600

    Use full class names in specs
    Originally I thought it was worth shortening the class names in the
    specs but now I think it makes it less readable since it 1) doesn't
    syntax highlight and 2) requires looking at the top of the spec to know
    what `ok` and `err` refer to.
    
    I considered aliasing the constants like I suggest in the README but
    that would pollute the top-level namespace in the other specs which
    seems undesirable.

--- spec/opted/result_spec.rb
+++ spec/opted/result_spec.rb
@@ -1,40 +1,37 @@
 RSpec.describe Opted::Result do
-  ok = Opted::Result::Ok
-  err = Opted::Result::Err
-
   describe "both result types" do
     it "have exactly the same interface" do
       object_methods = Object.instance_methods
-      ok_methods = ok.instance_methods - object_methods
-      err_methods = err.instance_methods - object_methods
+      ok_methods = Opted::Result::Ok.instance_methods - object_methods
+      err_methods = Opted::Result::Err.instance_methods - object_methods
       expect(ok_methods).to eql(err_methods)
     end
   end
 
-  describe ok do
+  describe Opted::Result::Ok do
     describe "#ok?" do
       it "is true" do
-        expect(ok.new(1).ok?).to eql(true)
+        expect(Opted::Result::Ok.new(1).ok?).to eql(true)
       end
     end
 
     describe "#unwrap!" do
       it "returns the value" do
-        expect(ok.new(1).unwrap!).to eql(1)
+        expect(Opted::Result::Ok.new(1).unwrap!).to eql(1)
       end
     end
 
     describe "#unwrap_err!" do
       it "raises an error" do
         expect do
-          ok.new("hello").unwrap_err!
+          Opted::Result::Ok.new("hello").unwrap_err!
         end.to raise_error(RuntimeError, "Called #unwrap_err! on Ok(\"hello\")")
       end
     end
 
     describe "#match" do
       it "runs the ok block" do
-        result = ok.new(1).match do
+        result = Opted::Result::Ok.new(1).match do
           ok { |value| value + 1 }
           err { fail "unreachable" }
         end
@@ -44,7 +41,7 @@ RSpec.describe Opted::Result do
 
       it "fails unless both ok and err block provided" do
         expect do
-          ok.new(1).match do
+          Opted::Result::Ok.new(1).match do
             ok { |value| value + 1 }
           end
         end.to raise_error(RuntimeError, "Must match on both ok and err results")
@@ -52,30 +49,30 @@ RSpec.describe Opted::Result do
     end
   end
 
-  describe err do
+  describe Opted::Result::Err do
     describe "#ok?" do
       it "is false" do
-        expect(err.new(:error).ok?).to eql(false)
+        expect(Opted::Result::Err.new(:error).ok?).to eql(false)
       end
     end
 
     describe "#unwrap!" do
       it "raises an error" do
         expect do
-          err.new(:whoops).unwrap!
+          Opted::Result::Err.new(:whoops).unwrap!
         end.to raise_error(RuntimeError, "Called #unwrap! on Err(:whoops)")
       end
     end
 
     describe "#unwrap_err!" do
       it "returns the error" do
-        expect(err.new(:whoops).unwrap_err!).to eql(:whoops)
+        expect(Opted::Result::Err.new(:whoops).unwrap_err!).to eql(:whoops)
       end
     end
 
     describe "#match" do
       it "runs the err block" do
-        result = err.new(:whoops).match do
+        result = Opted::Result::Err.new(:whoops).match do
           ok { fail "unreachable" }
           err { |error| "error is #{error}" }
         end
@@ -85,7 +82,7 @@ RSpec.describe Opted::Result do
 
       it "fails unless both ok and err block provided" do
         expect do
-          err.new(:whoops).match do
+          Opted::Result::Err.new(:whoops).match do
             err { |error| "error is #{error}" }
           end
         end.to raise_error(RuntimeError, "Must match on both ok and err results")
